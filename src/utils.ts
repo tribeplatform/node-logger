4c603ac3a90bac72c642a787806fa618cc1af182
Farnood Massoudi <fa.massoudi@gmail.com>
2022-11-10 20:36:27 -0500

    add and use chalk package

--- src/utils.ts
+++ src/utils.ts
@@ -1,31 +1,21 @@
+import * as chalk from 'chalk'
 import { LogLevel } from './interface'
 
-const NO_COLOR = '\u001b[39m'
-const MAGENTA = '\u001b[35m'
-const CYAN = '\u001b[36m'
-const GREEN = '\u001b[32m'
-const YELLOW = '\u001b[33m'
-const RED = '\u001b[31m'
-const WHITE = '\u001b[37m'
-
-const getLevelColor = (level: LogLevel | 'meta'): string => {
+export const getColorizedText = (text: string, level: LogLevel | 'meta') => {
   switch (level) {
     case 'debug':
-      return MAGENTA
+      return chalk.magenta(text)
     case 'verbose':
-      return CYAN
+      return chalk.cyan(text)
     case 'info':
-      return GREEN
+      return chalk.green(text)
     case 'warn':
-      return YELLOW
+      return chalk.yellow(text)
     case 'error':
-      return RED
+      return chalk.red(text)
     case 'meta':
-      return WHITE
+      return chalk.gray(text)
     default:
-      return GREEN
+      return chalk.green(text)
   }
 }
-
-export const getColorizedText = (text: string, level: LogLevel | 'meta') =>
-  `${getLevelColor(level)}${text}${NO_COLOR}`
